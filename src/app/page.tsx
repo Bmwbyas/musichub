3208215b164d5b885cf8cfb75ff05a6ebba7129d
alexAskerko <askerko50@gmail.com>
2023-12-11 19:36:56 +0300

    next config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,28 @@
+'use client'
+import {useEffect} from "react";
 import styles from './page.module.scss'
 
 
 export default function Home(): JSX.Element {
 
+    useEffect(() => {
+        fetch('https://api.music-hub.ru/portfolio',
+            {
+                method: "GET",
+                mode: "cors",
+                cache: "no-cache",
+                credentials: "omit"
+
+            })
+            .then((response) => {
+                console.log('bla')
+                return response.json();
+            })
+            .then((data) => {
+                console.log('bla2')
+                console.log(data);
+            });
+    }, [])
     return (
         <main className={styles['main']}>
             hello world
